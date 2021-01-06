de8eabd09de997234e6fa80321382025b00ce08b
parkerfoshay <40288355+parkerfoshay@users.noreply.github.com>
2021-01-06 14:40:32 -0700

    1

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
-const port = 3000;
+const port = 3001;
+
 
 const db = require("./queries");
 
